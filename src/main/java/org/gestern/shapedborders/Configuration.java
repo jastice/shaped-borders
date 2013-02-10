84bed384187590866832c43bdcba829d5fcb6665
Justin Kaeser <jast@zno.de>
2013-02-10 17:30:13 +0100

    make configuration readable/writable

--- src/main/java/org/gestern/shapedborders/Configuration.java
+++ src/main/java/org/gestern/shapedborders/Configuration.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Logger;
 
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.gestern.shapedborders.border.Border;
 
@@ -16,9 +17,9 @@ import org.gestern.shapedborders.border.Border;
  */
 public enum Configuration {
 	
-	/** Central configuration instance. */
+	/** Unique configuration instance. */
     CONF;
-    
+
     @SuppressWarnings("unused")
     private final Logger log = ShapedBorders.P.getLogger();
     
@@ -40,7 +41,25 @@ public enum Configuration {
      */
     @SuppressWarnings("unchecked")
     public void readConfig(FileConfiguration savedConfig) {
-        borders.putAll((Map<String, ? extends Border>) savedConfig.getValues(false));
+        ConfigurationSection borderSection = savedConfig.getConfigurationSection("borders");
+        if (borderSection!=null)
+            borders.putAll((Map<String, ? extends Border>) borderSection.getValues(false));
+        
+        this.knockback = savedConfig.getDouble("knockback");
+        this.whooshEffect = savedConfig.getBoolean("whooshEffect", true);
+        this.timerTicks = savedConfig.getInt("timerTicks", 5);
+    }
+    
+    /**
+     * Write config values to a configuration object.
+     * @param config
+     */
+    public void writeConfig(FileConfiguration config) {
+        config.set("borders", borders);
+        
+        config.set("knockback", knockback);
+        config.set("timerTicks", timerTicks);
+        config.set("whooshEffect", whooshEffect);
     }
     
     
